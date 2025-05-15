6261315fb4dcdc16d2a7676c114386b4ec16e168
Gudiao21 <lucianodantas53@gmail.com>
2025-05-14 21:38:08 -0300

    Criado a controller [aula2]

---
+++ src/main/java/dev/java10x/CadastroDeNinja/Controller.java
@@ -0,0 +1,15 @@
+package dev.java10x.CadastroDeNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // é uma 'annotation' para identificar uma classe como uma controladora entre o usuário e o BD, por exemplo.
+@RequestMapping // Trabalha junto com o '@RestController' para controladores, haja visto, que esta classe também mapeará rotas.
+public class Controller {
+
+    @GetMapping("/BoasVindas") // Na verdade, ocultamente, o nome completo da rota será 'localhost:8080/BoasVindas'
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota.";
+    }
+}
