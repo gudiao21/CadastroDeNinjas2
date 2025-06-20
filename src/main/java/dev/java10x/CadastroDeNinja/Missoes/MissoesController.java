ad150c5cbd4dce0f173a32f350e3c2cd035f3114
Gudiao21 <lucianodantas53@gmail.com>
2025-06-19 22:57:28 -0300

    feat: Criar as rotas do controller com mock

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
@@ -1,4 +1,32 @@
 package dev.java10x.CadastroDeNinja.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController // Usado para controlar a rota, ou seja, para dizer que toda a classe "MissoesController" será um controller!
+@RequestMapping("missoes") // Anotation para mapear as minha APIs, ou seja, as 'paths' após o 'localhost:8080'. "missoes" adicionará "missoes/..." em todas as rotas abaixo.
 public class MissoesController {
+
+    // Get -- Mandar uma requisição para mostrar as missões
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missões listadas com sucesso";
+    }
+
+    // Post -- Mandar uma requisição para criar as missões
+    @PostMapping("/criar") // O 'localhost:8080' será implícito
+    public String criarMissao() {
+        return "Missao criada com sucesso";
+    }
+
+    // Put -- Mandar uma requisição para alterar as missões
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada com sucesso";
+    }
+
+    // Delete -- Manda uma requisição para deletar uma missão
+    @DeleteMapping("/deletar")
+    public String deletarMisao() {
+        return "Missao deletada com sucesso";
+    }
 }
