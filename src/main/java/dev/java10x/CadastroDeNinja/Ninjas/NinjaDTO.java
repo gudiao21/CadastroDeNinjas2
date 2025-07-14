648598a99729c9352026eeb6c782c72465f90f22
Gudiao21 <lucianodantas53@gmail.com>
2025-07-13 21:27:21 -0300

    chore: Inclue a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaDTO.java
@@ -1,4 +1,29 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
+import dev.java10x.CadastroDeNinja.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data // Vem do lombok para gerar os 'getters' e 'setters'.
+@AllArgsConstructor // Vem do 'lombok' para gerar ocultamente o 'AllArgsContructor'.
+@NoArgsConstructor // Vem do 'lombok' para gerar ocultamente (por debaixo dos panos) o 'NoArgsContructor'.
 public class NinjaDTO {
+
+    private long id;
+
+    private String nome;
+
+    private String email;
+
+    private String imgUrl;
+
+    private int idade;
+
+    private String rank;
+
+    private MissoesModel missoes;
 }
+
+// OBS: O 'DTO' serve para tirar a responsabilidade da nossa Entidade. Fazendo assim, o 'DTO' conversar com o 'Service' e não mais o "Model" com o "Servicce", pois o 'DTO' é, praticamente, uma cópia do meu 'Model'.
+// OBS2: O 'DTO", fazendo uma analogia, seria uma camada a mais de proteção em minha aplicação.
