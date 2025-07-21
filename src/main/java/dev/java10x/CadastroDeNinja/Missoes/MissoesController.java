babb1fadce9f02c4ebfd414e7a8eec84e2959917
Gudiao21 <lucianodantas53@gmail.com>
2025-07-21 20:41:48 -0300

    feat: Inclue DTO como abstracao para a minha entidade de service e controller

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesController.java
@@ -10,35 +10,36 @@ import java.util.List;
 public class MissoesController {
 
     private MissoesService missoesService; // Faz a 'injeção de dependência' do 'missoesService.java', que faz parte da camada de 'Serviço', ou seja, aqui acorre a comunicação entre as camadas 'controller' e 'service' em uma Arquitetura de Camadas (Layered architecture).
+
     public MissoesController(MissoesService missoesService) { // Inicialização do construtor para 'MissoesService'. Lembrando que "Injeção de dependência" entrega para uma classe os objetos que ela precisa, ao invés da própria classe criar ou buscar esse(s) objeto(s).
         this.missoesService = missoesService;
     }
 
-    // Post -- Mandar uma requisição para criar a missão
+    // Adicionar missão (1 - CREATE)
     @PostMapping("/criar") // O 'localhost:8080' será implícito. DESCERIALIZAÇÃO, ou seja, o 'POST' faz uma serialização, mas inversa.
-    public MissoesModel criarMissao(@RequestBody MissoesModel missao) { // O '@RequestBody' é a annotation que pega um registro do usuário e o DESCERIALIZA para o BD, em Json.
+    public MissoesDTO criarMissao(@RequestBody MissoesDTO missao) { // O '@RequestBody' é a annotation que pega um registro do usuário e o DESCERIALIZA para o BD, em Json.
         return missoesService.criarMissao(missao); // Retorna a instância 'missoesService' para quem o chamou. O nome 'missao' é escolhido pelo dev.
     }
 
-    // Get -- Mandar uma requisição para mostrar TODAS as missões. SERIALIZAÇÃO
+    // Mostrar TODAS as missões. SERIALIZAÇÃO (2 - READ)
     @GetMapping("/listar")
-    public List<MissoesModel> listarMissoes() {
+    public List<MissoesDTO> listarMissoes() {
         return missoesService.listarMissoes(); /* "missoesService" é a instância do serviço que foi injetada via construtor ou com @Autowired. */
     }
 
-    // Mostrar Missão por id (READ)
+    // Mostrar Missão por id (2 - READ)
     @GetMapping("/listar/{id}") /* O '{id}' é chamado de 'path variable' */
-    public MissoesModel listarMissaoPorId(@PathVariable Long id) { // O '{id}' é chamado de 'path variable'
+    public MissoesDTO listarMissaoPorId(@PathVariable Long id) { // O '{id}' é chamado de 'path variable'
         return missoesService.listarMissoesPorId(id);
     }
 
-    // Put -- Mandar uma requisição para alterar as missões
-    @PutMapping("/alterar")
-    public String alterarMissao() {
-        return "Missao alterada com sucesso.";
+    // Alterar dados da Missão (3 - UPDATE)
+    @PutMapping("/alterar/{id}") // O '{id}' é chamado de 'path variable'.
+    public MissoesDTO atualizarMissaoPorId(@PathVariable Long id, @RequestBody MissoesDTO missaoAtualizada) {
+        return missoesService.atualizarMissao(id, missaoAtualizada);
     }
 
-    // Delete -- Manda uma requisição para deletar uma missão
+    // Delete -- Manda uma requisição para Deletar uma missão (5 - DELETE)
     @DeleteMapping("/deletar/{id}") // Criando o 'endpoint' para deletar
     public void deletarMissaoPorId(@PathVariable Long id) { // O método para deletar, sempre vai ser VOID.
         missoesService.deletarMissaoPorId(id); // Se utiliza da instância "missoesService" para chamar o método 'deletarMissaoPorId'.
--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesService.java
@@ -4,34 +4,55 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service // Transforma esta classe em uma camada de 'serviço'
 public class MissoesService { // Para 'service se comunicar com o 'repositório' é preciso eu fazer a injeção de dependência e a criação do construtor.
 
     private MissoesRepository missoesRepository; // Injeta a dependência do meu repositório 'MissoesRepository.java' no meu 'MissoesService.java'. É aqui que a camada 'Service' se comunica com a 'Repository'.
+    private MissoesMapper missoesMapper; // Inicia uma instância do missoesMapper.
 
-    public MissoesService(MissoesRepository missoesRepository) {
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) { // Optamos por criar este constructor, mas poderia ser pela annotation '@Autowired', sendo um ou outro, sabendo que há algumas diferenças entre eles. Aqui a dependência da linha 10 deste arquivo é inicializada efetivamente, ou seja, através deste contrutor. Inicializar por contrutor é mais indicado, porém pode ser por 'annotation' também (menos indicado).
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
     // Listar TODOS as minhas missões (implementa a lógica para listar todas as missões)
-    public List<MissoesModel> listarMissoes() { // Este método é chamado lá no "MissoesController"
-        return missoesRepository.findAll(); // Este método do repositório 'missoesRepository' é possível graças ao 'Service' se comunicar com o 'Repository' que por sua vez se comunica com o BD. O 'findAll' é equivalente ao 'SELECT' que simula as queries do BD (ex: H2, MySQL, ...)
+    public List<MissoesDTO> listarMissoes() { // Este método é chamado lá no "MissoesController"
+        List<MissoesModel> missoes = missoesRepository.findAll(); // Este método do repositório 'missoesRepository' é possível graças à linha 11, onde 'Service' se comunica com 'Repository' que por sua vez se comunica com o BD. O 'findAll' é equivalente ao 'SELECT * FROM TB_CADASTRO' da query do BD MySQL, ou seja, o 'fndAll' seria uma 'query' em forma de método. Aqui, efetivamente, ocorre a consulta usando o Model.
+        return missoes.stream()
+            .map(missoesMapper::map) // Para cada objeto MissoesModel que passa na esteira, ela chama o método map de um objeto chamado missoesMapper. Esse missoesMapper é uma classe que você criou (um Mapper/Converter) cuja única responsabilidade é saber como converter um MissoesModel em um MissoesDTO e vice-versa. Aqui, efetivamente, ocorre a conversão de cada MissoesModel em MissoesDTO. Veja que o 'Service' lida com o Model internamente, mas retorna DTOs para o Controller (camada de apresentação).
+                .collect(Collectors.toList());
+        //Antigo -> return missoesRepository.findAll(); // Este método do repositório 'missoesRepository' é possível graças ao 'Service' se comunicar com o 'Repository' que por sua vez se comunica com o BD. O 'findAll' é equivalente ao 'SELECT' que simula as queries do BD (ex: H2, MySQL, ...)
     } /* Ex. de como é feita esta consulta '(findall)' no sql: "SELECT + FROM TB_CADASTRO;", por exemplo. */
 
     // Listar missões por ID
-    public MissoesModel listarMissoesPorId(Long id) {
+    public MissoesDTO listarMissoesPorId(Long id) {
         Optional<MissoesModel> missaoPorId = missoesRepository.findById(id); /* 'Optional' porque o 'id' do ninja pode existir ou não */
-        return missaoPorId.orElse(null);
+        return missaoPorId.map(missoesMapper::map).orElse(null); // 'orElse' é um método que vem da própria classe "java.util.Optional", que significa que vai retornar o NinjaDTO (se o ninja foi encontrado e mapeado) ou Null (Se o ninja não foi encontrado). "ninjaPorId.map" transforma o 'NinjaModel' em um 'NinjaDTO', justamente para se retornar um 'DTO' e não um 'Model', expondo assim a segurança da aplicação. */
     } /* Ex. de como é feita esta consulta(findById) no sql: "SELECT * FROM TB_CADASTRO WHERE id=2;", por exemplo. */
 
     // Criar uma nova missão
-    public MissoesModel criarMissao(MissoesModel missao) {
-        return missoesRepository.save(missao); /* = "INSERT INTO TB_CADASTRO (id, nome, dificuldade) VALUE (1, "Naruto", "Fácil"). O 'return' retorna o objeto salvo para quem chamou o método 'criarmissao'. Quem registra no BD é o método 'save' do JPA. */
+    public MissoesDTO criarMissao(MissoesDTO missaoDTO) {
+        MissoesModel missao = missoesMapper.map(missaoDTO);
+        missao = missoesRepository.save(missao);
+        return missoesMapper.map(missao); /* = "INSERT INTO TB_MISSOES (id, nome, dificuldade) VALUE (1, "Naruto", "Fácil"). */
     }
 
     // Deletar Missao - Tem que ser um método VOID
     public void deletarMissaoPorId(Long id) {
         missoesRepository.deleteById(id); // = "DELETE FROM TB_MISSOES WHERE id = 2", por exemplo. Chama a instância do 'missoesRepository' para se utilizar do método JPA 'deleteById'.
+    }
+
+    // Atualizar missão
+    public MissoesDTO atualizarMissao(Long id, MissoesDTO missoesDTO) {
+        Optional<MissoesModel> missaoExistente = missoesRepository.findById(id);
+        if (missaoExistente.isPresent()) {
+            MissoesModel missaoAtualizada = missoesMapper.map(missoesDTO);
+            missaoAtualizada.setId(id); // Sobrescreve o 'id' antigo para o novo.
+            MissoesModel missaoSalva = missoesRepository.save(missaoAtualizada); //Sobrescreve e atualiza os dados atualizados da missao.
+            return missoesMapper.map(missaoSalva);
+        }
+        return null;
     }
 }
