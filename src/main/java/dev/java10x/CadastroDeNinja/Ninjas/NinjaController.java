0a2b8197e3290aa492ae99e0b8d868265526cc45
Gudiao21 <lucianodantas53@gmail.com>
2025-07-28 18:09:18 -0300

    Refactor: Refatora o controller para incluir o responseEntity nas minhas rotas e customizar a resposta das requisições do cliente.

--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaController.java
@@ -1,6 +1,8 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,31 +23,59 @@ public class NinjaController {
 
     // Adicionar Ninja (1 - CREATE)
     @PostMapping("/criar") // O método POST é o contrário do GET, pois ele pega o registro em forma de Json, do usário, e DESSERIALIZA(JSON -> Ojeto Java) para o bd, exceto o 'id' que é automático.
-    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja) { // O '@RequestBody' é a annotation que pega um registro do usuário e o DESERIALIZA para o BD, em Json (JavaScript object notation). Não é mais "NinjaModel" e sim "NinjaDTO" para conferir mais camadas a aplicação proporcionando mais segurança. Esta mudança é recomendada depois de se fazer o "NinjaMapper.java".
-        return ninjaService.criarNinja(ninja);
+    public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) { // O '@RequestBody' é a annotation que pega um registro do usuário e o DESERIALIZA para o BD, em Json (JavaScript object notation). Não é mais "NinjaModel" e sim "NinjaDTO" para conferir mais camadas a aplicação proporcionando mais segurança. Esta mudança é recomendada depois de se fazer o "NinjaMapper.java".
+        NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
+        return ResponseEntity.status(HttpStatus.CREATED) // Retorna para o SERVIDOR uma mensagem de status code de criado.
+                .body("Ninja criado com sucesso: " + novoNinja.getNome() + " (ID): " + novoNinja.getId()); // Retorna para o USUÁRIO uma mensagem personalizada de registro criado.
     }
 
     // Mostrar TODOS os Ninjas (2 - READ)
     @GetMapping("/listar") // O GET faz uma serialização do BD, em Json, para mostrar para o usuário
-    public List<NinjaDTO> listarNinjas() {
-        return ninjaService.listarNinjas(); // Retorna a instância do meu serviço chamado 'ninjaService', que por sua vez acessa o método 'listarNinjas()'. O 'ninjaService.listarNinjas' é possível aqui porque fizemos a injeção de dependência na linha 11 e a criação do construtor (linha 13 a 15). Como o 'service' está conectado ao 'repository', então por isto do acesso a query da JPA. "ninjaService" é a instância do serviço que foi injetada via construtor ou com @Autowired. Aqui, literalmente, ocorre a troca do DTO com o service.
+    public ResponseEntity<List<NinjaDTO>> listarNinjas() {
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas(); // Retorna a instância do meu serviço chamado 'ninjaService', que por sua vez acessa o método 'listarNinjas()'. O 'ninjaService.listarNinjas' é possível aqui porque fizemos a injeção de dependência na linha 11 e a criação do construtor (linha 13 a 15). Como o 'service' está conectado ao 'repository', então por isto do acesso a query da JPA. "ninjaService" é a instância do serviço que foi injetada via construtor ou com @Autowired. Aqui, literalmente, ocorre a troca do DTO com o service.
+        if (ninjas.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .build(); // Sem conteúdo, sem body
+        }
+        return ResponseEntity.ok(ninjas);
     }
 
     // Mostrar Ninja por id (2 - READ)
     @GetMapping("/listar/{id}") // O '{id}' é chamado de 'path variable'
-    public NinjaDTO listarNinjasPorId(@PathVariable Long id) { // Uma vez passado o 'id', o mesmo vai fazer parte do Path por causa da annotation "@PathVariable".
-        return ninjaService.listarNinjasPorId(id);
+    public ResponseEntity<?> listarNinjasPorId(@PathVariable Long id) {
+        NinjaDTO ninjaExistente = ninjaService.listarNinjasPorId(id);
+            if (ninjaExistente != null) {
+            return ResponseEntity.ok(ninjaExistente);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O ninja de Id: " + id + ", NÃO foi encontrado ou NÃO exite no banco de dados!");
+        }
     }
 
    // Alterar dados do Ninja (4 - UPDATE)
     @PutMapping("/alterar/{id}") // O 'Put' é uma mistura do 'getbyid' com o 'Post'
-    public NinjaDTO atualizarNinjaPorId(@PathVariable Long id, @RequestBody NinjaDTO ninjaAtualizado) { // 'ninjaAtualizado' seria o corpo da requisição alterado pelo usuário.
-        return ninjaService.atualizarNinja(id, ninjaAtualizado); // 'ninjaService' é a instância do meu 'service', aqui dentro do controller para que se possa ter acesso aos métodos do 'NinjaService.java' chamado 'atualizarNinja'.
+    public ResponseEntity<?> atualizarNinja(@PathVariable Long id, @RequestBody NinjaDTO ninjaDTO) { // 'ninjaAtualizado' seria o corpo da requisição alterado pelo usuário.
+        NinjaDTO ninjaExistente = ninjaService.listarNinjasPorId(id);
+
+        if (ninjaExistente != null) {
+            NinjaDTO ninjaAtualizado = ninjaService.atualizarNinja(id,ninjaDTO);
+            return ResponseEntity.ok(ninjaExistente);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O ninja de Id: " + id + ", NÃO foi encontrado ou não existe no banco de dados!");
+            }
     }
 
     // Deletar Ninja (5 - DELETE)
     @DeleteMapping("/deletar/{id}")
-    public void deletarNinjaPorId(@PathVariable Long id) {
-        ninjaService.deletarNinjaPorId(id);
+    public ResponseEntity<String> deletarNinjaPorId(@PathVariable Long id) {
+        if (ninjaService.listarNinjasPorId(id) != null) { // Se o ninja com o respectivo 'id' existir
+            ninjaService.deletarNinjaPorId(id); // Deletar ninja, pois a verificação se existe eu já fiz acima
+            return ResponseEntity.ok("Ninja com o ID " + id + " deletado com sucesso");
+       } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O ninja com o id " + id + " não encontrado.");
+            }
     }
 }
